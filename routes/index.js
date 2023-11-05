848cbb498a618e0cfc4360b8584297390acfe46f
Rafael Alcantara <rafael.alcantara.dev@gmail.com>
2023-11-05 01:19:52 -0300

    Created mogodb connection

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send('Hello World!')});
 
+router.get('/contacts', (req, res) => {res.send('Hello World!')});
+
 module.exports = router;
